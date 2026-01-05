1baedabb1adf19985ab9dccce1bf714e352eeb5f
Alvise Favero <66018479+alvi-se@users.noreply.github.com>
2026-01-05 16:46:05 +0100

    feat: test graphs

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
+from grakel import RandomWalkLabeled, graph_from_csv
+
 def main():
-    print("Hello from bioinformatics-project!")
+    g1 = graph_from_csv(
+        edge_files=(['./data/Eukaryotes/Eukaryotes_mc_0/edges_mc_0_aacu.csv'], False, False),
+        node_files=(['./data/Eukaryotes/Eukaryotes_mc_0/nodes_mc_0_aacu.csv'], False),
+    )
+    
+    g2 = graph_from_csv(
+        edge_files=(['./data/Eukaryotes/Eukaryotes_mc_0/edges_mc_0_aaf.csv'], False, False),
+        node_files=(['./data/Eukaryotes/Eukaryotes_mc_0/nodes_mc_0_aaf.csv'], False),
+    )
+
+
+    random_walk = RandomWalkLabeled(
+        lamda=1,
+        method_type='fast',
+        kernel_type='geometric',
+        p=1,
+    )
+    
+    random_walk.fit_transform(g1)
+    random_walk.transform(g2)
+
 
 
 if __name__ == "__main__":
